c3b701b2a868c4516726d38b1f2112f60e93154b
Forkk <forkk@forkk.net>
2014-05-06 14:55:28 -0500

    Add buttons to the version table.

---
+++ Handler/EditVersion.hs
@@ -0,0 +1,24 @@
+module Handler.EditVersion where
+
+import Import
+
+-- {{{ Add version
+
+getAddVersionR :: Text -> Handler Html
+getAddVersionR = error "Not yet implemented: getAddVersionR"
+
+postAddVersionR :: Text -> Handler Html
+postAddVersionR = error "Not yet implemented: postAddVersionR"
+
+-- }}}
+
+-- {{{ Edit version
+
+getEditVersionR :: Text -> Text -> Handler Html
+getEditVersionR = error "Not yet implemented: getEditVersionR"
+
+postEditVersionR :: Text -> Text -> Handler Html
+postEditVersionR = error "Not yet implemented: postEditVersionR"
+
+-- }}}
+
